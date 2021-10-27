e6c693572600adeb90a369efd98d1f39e2e48b6a
odedahay <odedahay@gmail.com>
2021-10-27 11:17:56 +0800

    Updated the Teams

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Team
+from django.utils.html import format_html
 
-# Register your models here.
+
+class TeamAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="35" style="border-radius: 50px"/>'.format(object.photo.url))
+    
+    thumbnail.short_description ='photo'
+
+    list_display = ('id', 'thumbnail','firstname', 'lastname', 'designation', 'created_date')
+    list_display_links = ('id', 'thumbnail', 'firstname')
+    search_fields = ('firstname', 'lastname',)
+    list_filter = ('designation',)
+
+admin.site.register(Team, TeamAdmin)
